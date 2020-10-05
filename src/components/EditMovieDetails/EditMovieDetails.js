1f2abb4ffde4f012afdaf482b39e46ae5251a32a
Derek Williamson <derekwilliamson22@gmail.com>
2020-10-04 22:02:02 -0500

    formatting the edit details page

--- src/components/EditMovieDetails/EditMovieDetails.js
+++ src/components/EditMovieDetails/EditMovieDetails.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class EditMovieDetails extends Component{
-        render(){
-            return(
-                <div>
-                    <h1>EditDetails</h1>
 
-                </div>
-            )
-        }
+
+class EditMovieDetails extends Component{
+
+
+  render(){
+    return(
+        <div>
+          <h1>EditDetails</h1>
+        </div>
+    )
+  }
 }
 
 export default connect()(EditMovieDetails);
